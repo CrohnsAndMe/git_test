1e229d93e2b346473c6bb14befede7d26a5df9e8
CrohnsAndMe <s013010c@student.staffs.ac.uk>
2014-10-02 12:33:09 +0100

    Added Player class

---
+++ App1/App1/App1.Windows/Player.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App1
+{
+    class Player
+    {
+
+        public Player()
+        {
+
+        }
+
+        public bool CheckCollision() { return true; }
+
+        public void Jump() { }
+
+        public void Move(string dir) {  }
+
+        public void AddScore(int value) { }
+    }
+}
